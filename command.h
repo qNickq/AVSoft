f128559104a232db242e23032b8e93b0c513f5d5
qNickq <qNickq@bk.ru>
2019-11-24 00:38:26 +0300

    Destructor is virtual now.

--- command.h
+++ command.h
@@ -1,18 +1,22 @@
 #ifndef COMMAND_H
 #define COMMAND_H
+#include "company.h"
 
 class Command
 {
 
 public:
-    Command();
+    Command(Company * company = nullptr, Department * department = nullptr, Employee* employee = nullptr);
 
     virtual void execute() = 0;
     virtual void undo() = 0;
-    virtual void redo() = 0;
-     ~Command();
-protected:
 
+    virtual ~Command();
+
+protected:
+    Company * _company;
+    Department * _department;
+    Employee* _employee;
 };
 
 #endif // COMMAND_H
